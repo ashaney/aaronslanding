12194cc808cf77da6b2c7e4ee1ab7f1da5470386
aaron <aar0labs@pm.me>
2024-10-09 13:57:42 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body className={`${firaCode.className} bg-black text-white`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
