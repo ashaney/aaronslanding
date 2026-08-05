e0d8b26cfb784c75272b6c596cbbc24adc773a80
Aaron <ashaney@users.noreply.github.com>
2026-08-05 13:44:37 -0500

    remove 5things

--- app/page.tsx
+++ app/page.tsx
@@ -77,11 +77,6 @@ export default function Home() {
           <h2 className="mb-4">personal projects ⁘</h2>
           <ul className="space-y-2">
             <li>
-              <Link href="https://fivethings.today" className="hover:underline">
-                5 things
-              </Link>
-            </li>
-            <li>
               <Link
                 href="https://well-traveled.app/"
                 className="hover:underline"
